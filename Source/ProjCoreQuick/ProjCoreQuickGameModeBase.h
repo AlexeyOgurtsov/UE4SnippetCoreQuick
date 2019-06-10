b008932b1dd7017b04599e1c89c7ee1c8140f7fd
alexey_eng <alexey_eng@mail.ru>
2019-06-10 17:15:51 +0500

    initial

---
+++ Source/ProjCoreQuick/ProjCoreQuickGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjCoreQuickGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJCOREQUICK_API AProjCoreQuickGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
